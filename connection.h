2a31106878eb3e1c09fb7aa24f71f2b6717a5137
sammieliu <sammieliu@tencent.com>
2017-11-26 22:20:56 +0800

    aa

---
+++ connection.h
@@ -0,0 +1,15 @@
+/**
+* @file connection.h
+* @brief
+* @version 1.0
+* @date 10/15/2017 03:13:21 PM
+* @author sammieliu,sammieliu@tencent.com 
+* @copyright Copyright 1998 - 2017 Tencent. All Rights Reserved.
+*/
+/*
+* File connection.h
+*/
+
+#define SOCKET_NAME "/tmp/9Lq7BNBnBycd6nxy.socket"
+#define BUFFER_SIZE 12
+
